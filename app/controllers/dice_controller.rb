922e78b4e54c5d226fa3b7dd0ae0e00b914399dd
jessie5557 <ychen38@chicagobooth.edu>
2025-02-11 07:33:32 +0000

    done

---
+++ app/controllers/dice_controller.rb
@@ -0,0 +1,21 @@
+class DiceController < ApplicationController
+  def roll
+    
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+    
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@sides)
+      @rolls.push(die)
+    end 
+    
+    render({:template => "dice_templates/roll"})
+
+  end
+
+  def home
+    render({:template => "dice_templates/home"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+get "dice/:number_of_dice/:how_many_sides", {:controller => "dice", :action => "roll"}
+
+get "/", {:controller => "dice", :action => "home"}
 
 end
